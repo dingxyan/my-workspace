7a52647831c363af4292c52c0d910c5ed484f43f
dingxiaoyan <dingxiaoyan@le.com>
2018-02-01 14:50:51 +0800

    add

--- src/js/Greeter.js
+++ src/js/Greeter.js
@@ -1,11 +1,6 @@
 // Greeter.js
 module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
-  return greet;
+	var greet = document.createElement('div');
+	greet.textContent = "Hi there and greetings!";
+	return greet;
 };
-
-作者：zhangwang
-链接：https://www.jianshu.com/p/42e11515c10f
-來源：简书
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import './css/main.css'
+
+const greeter = require("./js/Greeter.js");
 function test(){
 	let a = 1;
-	console.log(a)
+	console.log(a,1)
+	document.getElementById("greeter").appendChild(greeter());
 }
 test();
